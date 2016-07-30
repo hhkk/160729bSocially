99740652e5bfad42bad8a30952055453d8d0cc56
hhkk <hank.khang@gmail.com>
2016-07-30 04:21:43 -0400

    sdssdsds

---
+++ client/imports/party-details/party-details.ts
@@ -0,0 +1,40 @@
+import { Component, NgZone } from '@angular/core';
+import { ActivatedRoute, ROUTER_DIRECTIVES } from '@angular/router';
+import { Tracker } from 'meteor/tracker';
+import {Parties} from '../../../collections/parties.ts';
+
+import template from './party-details.html';
+
+@Component({
+  selector: 'party-details',
+  template,
+  directives: [ROUTER_DIRECTIVES]
+})
+export class PartyDetails {
+  partyId: string;
+  party: Object;
+
+  constructor(private route: ActivatedRoute, private ngZone: NgZone) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.partyId = params['partyId'];
+
+      Tracker.autorun(() => {
+        this.ngZone.run(() => {
+          this.party = Parties.findOne(this.partyId);
+        });
+      });
+    });
+  }
+
+  saveParty(party) {
+    Parties.update(party._id, {
+      $set: {
+        name: party.name,
+        description: party.description,
+        location: party.location
+      }
+    });
+  }
+}
---
+++ server/main.ts
@@ -0,0 +1,5 @@
+import {loadParties} from './load-parties.ts';
+import {Meteor} from 'meteor/meteor';
+
+Meteor.startup(loadParties);
+
